e1a7a38cd46c62993b28b5509b0721b7a3f28fa6
gastonAr1985 <gastonarbert@gmail.com>
2022-06-28 17:32:45 -0300

    primer

---
+++ src/components/Respuestas.vue
@@ -0,0 +1,62 @@
+<template >
+
+  <section class="src-components-respuestas">
+    <div class="jumbotron">
+
+    <h1>Respuestas</h1>
+    <div v-if="respuestas.length" class="table-responsive">
+      <table class="table table-dark">
+        <tr>
+          
+          <th>Pregunta Numero</th>
+          <th>Respuesta</th>
+
+        </tr>
+        <tr v-for="(respuesta,index) in respuestas" :key="index">
+
+          <th>{{respuesta.pregunta}}</th>
+          <th>{{respuesta.respuesta}}</th>
+
+        </tr>
+      </table>
+    </div>
+    </div>
+  </section>
+
+</template>
+
+<script >
+
+  export default  {
+    name: 'src-components-respuestas',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+        respuestas:[
+          {pregunta: 1, respuesta: 'A'},
+          {pregunta: 2, respuesta: 'A'},
+
+        ]
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-respuestas {
+
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import './form.js'
+
+import { router } from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
